6034f71c553fb44b736c06bcea6eba281335772c
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2025-09-11 08:15:56 +0530

    embedding

--- test.py
+++ test.py
@@ -2,7 +2,5 @@
 
 
 
-from openai import OpenAI
 
-client = OpenAI(api_key="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")
-print(client.models.list())
+
